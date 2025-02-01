3f56f0444dfcfcba09ed9ab00dbb0478028c58f3
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2025-02-01 21:14:39 +0530

    minor changes

--- app/api/quiz/[id]/route.ts
+++ app/api/quiz/[id]/route.ts
@@ -1,18 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
-// Create Prisma client instance
 const prisma = new PrismaClient();
-
-// Define GET request handler
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } } // Adjust type for params
+  context: { params: { id: string } }
 ) {
   try {
-    const { id } = params; // Destructure the id from params
-
-    // Fetch the quiz from the database using Prisma
+    const { id } = context.params;
     const quiz = await prisma.quiz.findUnique({
       where: { id },
       select: {
